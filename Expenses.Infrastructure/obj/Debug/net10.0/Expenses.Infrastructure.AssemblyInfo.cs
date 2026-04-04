2ad8461f4f2d36719cd42139396a63066faaa14b
LilAguh <aguezeochoa@gmail.com>
2026-04-04 01:17:43 -0300

    chore: Infraestructure implementa interfaces definidas en Application

--- Expenses.Infrastructure/obj/Debug/net10.0/Expenses.Infrastructure.AssemblyInfo.cs
+++ Expenses.Infrastructure/obj/Debug/net10.0/Expenses.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expenses.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c714d7f96501d82ec6a40df23e12c61861e1461d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755bf48c057438db285f063700da879e2c20925f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expenses.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expenses.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
