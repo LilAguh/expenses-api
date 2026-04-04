60cc04858ce61ae962357157aa9cc59d9f7a3d7b
LilAguh <aguezeochoa@gmail.com>
2026-04-04 02:10:08 -0300

    feat: se define el modelo central del negocio

--- Expenses.Domain/Entities/Expense.cs
+++ Expenses.Domain/Entities/Expense.cs
@@ -3,9 +3,15 @@ namespace Expenses.Doomain.Entities;
 public class Expense
 {
   public int Id {get; set;}
+  //numero de id
   public string Description {get; set;} = string.Empty;
+  //string de descipcion
   public int Amount {get; set;}
+  //numero de monto
   public string Category {get; set;} = string.Empty;
+  //string de categoria
   public string Currency {get; set;} = "ARS";
+  //string de tipo de moneda
   public DateTime CreatedAt {get; set;}
+  //fecha de creacion del ticket
 }
